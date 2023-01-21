37c0c904cc8b41e3c8170a89e038b79e45a44ae0
Denys Havryliak <den4iklvivua@gmail.com>
2023-01-21 23:07:15 +0200

    Initial Commit

---
+++ NavigationStackDemo/NavigationStackDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackDemoApp.swift
+//  NavigationStackDemo
+//
+//  Created by Denys on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
